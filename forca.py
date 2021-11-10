a4a1dca7f36ba824d22fe7b4dc4d736ce57cc30f
Daniel Penno <danielpenno@gmail.com>
2021-11-10 01:56:42 -0300

    refatorando e melhorando

--- forca.py
+++ forca.py
@@ -2,10 +2,21 @@ import random
 
 
 def jogar():
+
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    # restante do código omitido
+
+
+def imprime_mensagem_abertura():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+
+def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -18,39 +29,13 @@ def jogar():
     numero = random.randrange(0, len(palavras))
 
     palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
 
-    enforcou = False
-    acertou = False
-    erros = 0
-
-    print(letras_acertadas)
-
-    while (not acertou and not enforcou):
-
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
-
-        if (chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
-
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
-
-    if (acertou):
-        print("Você ganhou!")
-    else:
-        print("Você perdeu!")
-
-    print("Fim do jogo")
+    return palavra_secreta
 
 
-if(__name__ == "__main__"):
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+
+if (__name__ == "__main__"):
     jogar()
