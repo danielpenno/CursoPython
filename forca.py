9ae9bd810ea5013ea9af8ec31853e872dc181de1
Daniel Penno <danielpenno@gmail.com>
2021-11-10 01:13:54 -0300

    aula listas

--- forca.py
+++ forca.py
@@ -4,14 +4,23 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
-
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
 
-    while(not acertou and not enforcou):
-        print("Jogando...")
+    print(letras_acertadas)
 
-    print("Fim do jogo")
+    while (not acertou and not enforcou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
 
 if(__name__ == "__main__"):
--- jogos.py
+++ jogos.py
@@ -6,15 +6,13 @@ def escolhe_jogo():
     print("*********************************")
     print("*******Escolha o seu jogo!*******")
     print("*********************************")
-
     print("(1) Forca (2) Adivinhação")
 
-    jogo = int(input("Qual jogo? "))
-
-    if(jogo == 1):
+    jogo = int(input("Qual jogo você quer jogar? "))
+    if (jogo == 1):
         print("Jogando forca")
         forca.jogar()
-    elif(jogo == 2):
+    elif (jogo == 2):
         print("Jogando adivinhação")
         adivinhacao.jogar()
 
