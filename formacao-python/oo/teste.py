23ade87b8294cc322adbc20d1cda0d0f43e93e00
Daniel Penno <danielpenno@gmail.com>
2021-11-12 17:38:14 -0300

    Aula01-Orientacao

---
+++ formacao-python/oo/teste.py
@@ -0,0 +1,24 @@
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular,
+             "saldo": saldo, "limite": limite}
+    return conta
+
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
+
+
+if(__name__ == "__main__"):
+    conta = cria_conta(123, "Nico", 55.0, 1000.0)
+    deposita(conta, 15.0)
+    extrato(conta)
+    saca(conta, 20.0)
+    extrato(conta)
