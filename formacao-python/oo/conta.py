9bc71876efb706cd6c7fb09db2e5bb0c3ba349f8
Daniel Penno <danielpenno@gmail.com>
2021-11-13 20:34:32 -0300

    Métodos.

--- formacao-python/oo/conta.py
+++ formacao-python/oo/conta.py
@@ -1,7 +1,17 @@
 class Conta:
+
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto...{}".format(self))
+        print("Construindo objeto ... {}".format(self))
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
