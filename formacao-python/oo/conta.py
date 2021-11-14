6bce42233b13db303ae74df5bf03ebf1881d9b4b
Daniel Penno <danielpenno@gmail.com>
2021-11-13 22:11:48 -0300

    Propriedades.

--- formacao-python/oo/conta.py
+++ formacao-python/oo/conta.py
@@ -1,21 +1,36 @@
 class Conta:
-
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto ... {}".format(self))
+        print("Construindo objeto... {}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
--- formacao-python/oo/teste.py
+++ formacao-python/oo/teste.py
@@ -13,12 +13,4 @@ def saca(conta, valor):
 
 
 def extrato(conta):
-    print("Saldo é {}".format(conta["saldo"]))
-
-
-if(__name__ == "__main__"):
-    conta = cria_conta(123, "Nico", 55.0, 1000.0)
-    deposita(conta, 15.0)
-    extrato(conta)
-    saca(conta, 20.0)
-    extrato(conta)
+    print("Saldo {}".format(conta["saldo"]))
