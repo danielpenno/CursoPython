43a5251f25a2c54777670a6b53bfccbf82941f82
Daniel Penno <danielpenno@gmail.com>
2021-11-15 21:08:00 -0300

    RegEx

--- formacao-python/extrator-url/extrator_url.py
+++ formacao-python/extrator-url/extrator_url.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -11,7 +14,13 @@ class ExtratorURL:
 
     def valida_url(self):
         if not self.url:
-            raise ValueError("A URL está vazia.")
+            raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile(
+            "(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
