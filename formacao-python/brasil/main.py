7ba9267d61612b2905605754ab85a949363c7365
Daniel Penno <danielpenno@gmail.com>
2021-11-16 17:31:50 -0300

    Máscara CNPJ.

--- formacao-python/brasil/main.py
+++ formacao-python/brasil/main.py
@@ -1,6 +1,8 @@
 from cpf_cnpj import CpfCnpj
-from validate_docbr import CPF, CNPJ
+from validate_docbr import CNPJ
 
 exemplo_cnpj = "35379838000112"
+exemplo_cpf = "32007832062"
 
-documento = CpfCnpj(exemplo_cnpj, 'cnpj')
+documento = CpfCnpj(exemplo_cpf, 'cpf')
+print(documento)
