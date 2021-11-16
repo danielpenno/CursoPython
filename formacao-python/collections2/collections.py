abf00a517af10d525180bb067853288faa340be1
Daniel Penno <danielpenno@gmail.com>
2021-11-16 16:11:19 -0300

    Dicionários.

--- formacao-python/collections2/collections.py
+++ formacao-python/collections2/collections.py
@@ -1,4 +1,4 @@
-usuarios_data_science = [15, 23, 43, 56]
+""" usuarios_data_science = [15, 23, 43, 56]
 usuarios_machine_learning = [13, 23, 56, 42]
 
 assistiram = usuarios_data_science.copy()
@@ -6,3 +6,22 @@ assistiram.extend(usuarios_machine_learning)
 print(assistiram)
 
 print(set(assistiram))
+
+usuarios = {1, 5, 76, 34, 52, 13, 17}
+print(len(usuarios))
+
+usuarios.add(765)
+print(len(usuarios))
+
+print(usuarios) """
+
+aparicoes = {
+    "Guilherme": 1,
+    "cachorro": 2,
+    "nome": 2,
+    "vindo": 1
+}
+
+print(type(aparicoes))
+
+print(aparicoes["Guilherme"])
