76212f3c8b2b43c7b7543f6de400097d7aade48b
Daniel Penno <danielpenno@gmail.com>
2021-11-18 14:38:48 -0300

    Excecoes e refatoração.

--- formacao-python/test_leilao.py
+++ formacao-python/test_leilao.py
@@ -1,18 +1,16 @@
 from unittest import TestCase
-from testes.dominio import Usuario, Lance, Leilao
+from leilao.dominio import Usuario, Lance, Leilao
+from leilao.excecoes import LanceInvalido
 
 
 class TestLeilao(TestCase):
-
-    # test_quando_adicionados_em_ordem_crescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance
-
     def setUp(self):
-        self.gui = Usuario('Gui')
+        self.gui = Usuario('Gui', 500.0)
         self.lance_do_gui = Lance(self.gui, 150.0)
         self.leilao = Leilao('Celular')
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        yuri = Usuario('Yuri')
+        yuri = Usuario('Yuri', 500.0)
         lance_do_yuri = Lance(yuri, 100.0)
 
         self.leilao.propoe(lance_do_yuri)
@@ -25,9 +23,8 @@ class TestLeilao(TestCase):
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
     def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
-
-        with self.assertRaises(ValueError):
-            yuri = Usuario('Yuri')
+        with self.assertRaises(LanceInvalido):
+            yuri = Usuario('Yuri', 500.0)
             lance_do_yuri = Lance(yuri, 100.0)
 
             self.leilao.propoe(self.lance_do_gui)
@@ -40,9 +37,9 @@ class TestLeilao(TestCase):
         self.assertEqual(150.0, self.leilao.maior_lance)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
-        yuri = Usuario('Yuri')
+        yuri = Usuario('Yuri', 500.0)
         lance_do_yuri = Lance(yuri, 100.0)
-        vini = Usuario('Vini')
+        vini = Usuario('Vini', 500.0)
 
         lance_do_vini = Lance(vini, 200.0)
 
@@ -63,7 +60,7 @@ class TestLeilao(TestCase):
         self.assertEqual(1, quantidade_de_lances_recebido)
 
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
-        yuri = Usuario('Yuri')
+        yuri = Usuario('Yuri', 500.0)
         lance_do_yuri = Lance(yuri, 200.0)
 
         self.leilao.propoe(self.lance_do_gui)
@@ -76,6 +73,6 @@ class TestLeilao(TestCase):
     def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
         lance_do_gui200 = Lance(self.gui, 200.0)
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(LanceInvalido):
             self.leilao.propoe(self.lance_do_gui)
             self.leilao.propoe(lance_do_gui200)
